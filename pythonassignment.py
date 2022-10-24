2953c6e25a3ec378aed141662efdebe8cb6f848b
Nyawira Gitaka <nyawira.gitaka@gmail.com>
2022-10-24 08:56:31 +0300

    Additions

--- pythonassignment.py
+++ pythonassignment.py
@@ -5,9 +5,9 @@ for number in range (1, 31):
 for number in range (1, 31):
     if number % 2 != 0:
        print(f"Odd {number}") 
-
-number = 1
-while number <= 31:
-    if number % 2 == 0:
-        print (f"{number}")
         
+number = 2
+while number <= 31:
+    print(number)
+    number = number + 2
+
