cfa849eebf5e9a8803d9864ba80b793820688753
Nyawira Gitaka <nyawira.gitaka@gmail.com>
2022-10-24 08:31:55 +0300

    Python basic operations

---
+++ pythonassignment.py
@@ -0,0 +1,13 @@
+for number in range (1, 31):
+    if number % 2 == 0:
+       print(f"Even {number}") 
+
+for number in range (1, 31):
+    if number % 2 != 0:
+       print(f"Odd {number}") 
+
+number = 1
+while number <= 31:
+    if number % 2 == 0:
+        print (f"{number}")
+        
